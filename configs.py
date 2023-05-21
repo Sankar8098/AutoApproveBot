d5103f564af11a9715a5cbac8960bbfd56a8aef9
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-05-21 20:45:27 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "5849628330:AAHjBZeXFAM9f2LkgH4OprLoyPycg9VQbhE")
     FSUB = getenv("FSUB", "SK_MoviesOffl")
     CHID = int(getenv("CHID", "-1001828551401"))
-    SUDO = list(map(int, getenv("SUDO").split(5821871362)))
+    SUDO = list(map(int, getenv("SUDO").split("5821871362"))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://nakflixbot:alpha3720@cluster0.qgybxbu.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
